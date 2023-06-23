8dc00b0ef4223e039501c0ee357daf2db5f76ac3
vinngo <ngovincent9593@gmail.com>
2023-06-23 10:46:01 -0700

    is this not badass????

--- Pomochromo/script.js
+++ Pomochromo/script.js
@@ -7,58 +7,54 @@ var reps = 0;
 var minutesInterval;
 var secondsInterval;
 
-function start() {
-    
+function minutesTimer() {
+    if (minutes != 0){
+        minutes = minutes - 1;
+    }
+}
+
+function secondsTimer() {
+    seconds = seconds - 1;
+    document.getElementById("timer").textContent = minutes + ":" + seconds;
+    console.log(minutes + ":" + seconds);
+    if (seconds <= 0){
+        if (minutes <= 0){
+            clearInterval(minutesInterval);
+            clearInterval(secondsInterval);
+            reps += 1;
+            pomodoro();
+        }
+        seconds = 60;
+    }
+}
+
+function pomodoro(){
+    if (loop[reps] === "work"){
+        minutes = 24;
+        seconds = 0;
+        minutesInterval = setInterval(minutesTimer, 60000);
+        secondsInterval = setInterval(secondsTimer, 1000);
+    } else if (loop[reps] === "break"){
+        minutes = 4;
+        seconds = 0;
+        minutesInterval = setInterval(minutesTimer, 60000);
+        secondsInterval = setInterval(secondsTimer, 1000);
+    } else {
+        minutes = 14;
+        seconds = 0;
+        minutesInterval = setInterval(minutesTimer, 60000);
+        secondsInterval = setInterval(secondsTimer, 1000);
+    }      
+}
+
+function start() {    
     minutes = 24;
     seconds = 59;
-
     //method to update time on the screen
     document.getElementById("timer").textContent = minutes + ":" + seconds;
 
     minutesInterval = setInterval(minutesTimer, 60000)
     secondsInterval = setInterval(secondsTimer, 1000)
-
-    function minutesTimer() {
-        if (minutes != 0){
-            minutes = minutes - 1;
-        }
-    }
-
-    function secondsTimer() {
-        seconds = seconds - 1;
-        document.getElementById("timer").textContent = minutes + ":" + seconds;
-        console.log(minutes + ":" + seconds);
-        if (seconds <= 0){
-            if (minutes <= 0){
-                clearInterval(minutesInterval);
-                clearInterval(secondsInterval);
-                reps += 1;
-                pomodoro();
-            }
-            seconds = 60;
-        }
-    }
-
-
-
-    function pomodoro(){
-        if (loop[reps] === "work"){
-            minutes = 24;
-            seconds = 0;
-            minutesInterval = setInterval(minutesTimer, 60000);
-            secondsInterval = setInterval(secondsTimer, 1000);
-        } else if (loop[reps] === "break"){
-            minutes = 4;
-            seconds = 0;
-            minutesInterval = setInterval(minutesTimer, 60000);
-            secondsInterval = setInterval(secondsTimer, 1000);
-        } else {
-            minutes = 14;
-            seconds = 0;
-            minutesInterval = setInterval(minutesTimer, 60000);
-            secondsInterval = setInterval(secondsTimer, 1000);
-        }      
-    }
 }
 
 function pause(){
@@ -73,9 +69,31 @@ function reset(){
   minutes = 25;
   seconds = 0;
   document.getElementById("timer").textContent = minutes + ":" + seconds;
+  document.getElementById("start").textContent = "Start";
 }
 
 function resume(){
   minutesInterval = setInterval(minutesTimer, 60000);
   secondsInterval = setInterval(secondsTimer, 1000);
+}
+
+function alternate(){
+    if (document.getElementById("start").textContent === "Start" || document.getElementById("start").textContent === "Resume"){
+        document.getElementById("start").textContent = "Pause";
+    } else {
+        document.getElementById("start").textContent = "Resume";
+    }
+}
+
+function startTimer(){
+    if (document.getElementById("start").textContent === "Start"){
+        start();
+        alternate();
+    } else if (document.getElementById("start").textContent === "Resume"){
+        resume();
+        alternate();
+    } else {
+        pause();
+        alternate();
+    }
 }
