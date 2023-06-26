e64da07f5e9237a9663a84916ccf1d0f1752e77b
vinngo <ngovincent9593@gmail.com>
2023-06-26 08:48:27 -0700

    Changed layout and added new method (in comments)

--- Pomochromo/script.js
+++ Pomochromo/script.js
@@ -9,8 +9,90 @@ var secondsInterval;
 document.addEventListener("DOMContentLoaded", init, false);
 
 function init() {
+  //document.getElementById("Pomodoro").style.backgroundColor = "rgb(108, 145, 118)"
   document.getElementById("reset").addEventListener("click", reset, true);
   document.getElementById("start").addEventListener("click", startTimer, true);
+  document.getElementById("Pomodoro").addEventListener("click", switchModePomodoro, true);
+  document.getElementById("ShortBreak").addEventListener("click", switchModeBreak, true);
+  document.getElementById("LongBreak").addEventListener("click", switchModeLong, true);
+}
+
+function switchModePomodoro() {
+    /*
+    if (loop[reps] === "work"){
+
+    } else {
+        changeBackground("Pomodoro");
+    }
+    */
+    clearInterval(minutesInterval);
+    clearInterval(secondsInterval);
+    if (loop[reps] === "work") {
+        minutes = 25;
+        seconds = 0;
+    } else if (loop[reps] === "break") {
+        reps = 0;
+        minutes = 25;
+        seconds = 0;
+    } else {
+        reps = 0;
+        minutes = 25;
+        seconds = 0;
+    }
+    document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
+    document.getElementById("start").textContent = "Start";
+}
+
+function switchModeBreak() {
+    /*
+    if (loop[reps] === "break"){
+
+    } else {
+        changeBackground("ShortBreak");
+    }
+    */
+    clearInterval(minutesInterval);
+    clearInterval(secondsInterval);
+    if (loop[reps] === "work"){
+        reps = 1;
+        minutes = "0" + 5;
+        seconds = 0;
+    } else if (loop[reps] === "break") {
+        minutes = "0" + 5;
+        seconds = 0;
+    } else {
+        reps = 1;
+        minutes = "0" + 5;
+        seconds = 0;
+    }
+    document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
+    document.getElementById("start").textContent = "Start";
+}
+
+function switchModeLong() {
+    /*
+    if (loop[reps] === "longbreak"){
+        
+    } else {
+        changeBackground("LongBreak");
+    }
+    */
+    clearInterval(minutesInterval);
+    clearInterval(secondsInterval);
+    if (loop[reps] === "work"){
+        reps = 5;
+        minutes = 15;
+        seconds = 0;
+    } else if (loop[reps] === "break") {
+        reps = 5;
+        minutes = 15;
+        seconds = 0;
+    } else {
+        minutes = 15;
+        seconds = 0;
+    }
+    document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
+    document.getElementById("start").textContent = "Start";
 }
 
 function minutesTimer() {
@@ -30,7 +112,11 @@ function secondsTimer() {
     if (minutes <= 0) {
       clearInterval(minutesInterval);
       clearInterval(secondsInterval);
-      reps += 1;
+      if (reps == 5) {
+        reps = 0;
+      } else {
+        reps += 1;
+      }
       pomodoro();
     }
     seconds = 60;
@@ -61,7 +147,7 @@ function pomodoro() {
 }
 
 function start() {
-  minutes = 24;
+  minutes -= 1;
   seconds = 59;
   //method to update time on the screen
   document.getElementById("timer").textContent = minutes + ":" + seconds;
@@ -113,3 +199,25 @@ function startTimer() {
     alternate();
   }
 }
+
+
+/*   really broken, i should fix this sometime.
+function changeBackground(id) {
+    var flag;
+    var buttons = document.getElementsByClassName("option");
+    for (let i = 0; i < buttons.length; i++){
+        if (buttons.item(i).style.backgroundColor = "rgb(108, 145, 118)"){
+            flag = true;
+        }
+    }
+    if (flag){
+
+    } else {
+        if (document.getElementById(id).style.backgroundColor === "transparent"){
+            document.getElementById(id).style.backgroundColor = "rgb(108, 145, 118)"; 
+        } else {
+            document.getElementById(id).style.backgroundColor = "transparent";
+        }
+    }
+}
+*/
