5c984dd737a47e1d1d68492f4bc1e394e11801c8
vinngo <ngovincent9593@gmail.com>
2023-06-29 13:21:42 -0700

    add images, new windows, change ui, etc.

---
+++ Pomochromo/blocker.js
@@ -0,0 +1,120 @@
+const generateSTYLES = () => {
+    return `<style>@import url(https://fonts.googleapis.com/css?family=opensans:500);
+    body {
+      background: #73a580;
+      color: #fff;
+      font-family: "Open Sans", sans-serif;
+      max-height: 700px;
+      overflow: hidden;
+    }
+    .c {
+      text-align: center;
+      display: block;
+      position: relative;
+      width: 80%;
+      margin: 100px auto;
+    }
+    ._404 {
+      font-size: 220px;
+      position: relative;
+      display: inline-block;
+      z-index: 2;
+      height: 250px;
+      letter-spacing: 15px;
+    }
+    ._1 {
+      text-align: center;
+      display: block;
+      position: relative;
+      letter-spacing: 12px;
+      font-size: 4em;
+      line-height: 80%;
+    }
+    ._2 {
+      text-align: center;
+      display: block;
+      position: relative;
+      font-size: 20px;
+    }
+    .text {
+      font-size: 70px;
+      text-align: center;
+      position: relative;
+      display: inline-block;
+      margin: 19px 0px 0px 0px;
+      /* top: 256.301px; */
+      z-index: 3;
+      width: 100%;
+      line-height: 1.2em;
+      display: inline-block;
+    }
+    
+   
+    .right {
+      float: right;
+      width: 60%;
+    }
+    
+    hr {
+      padding: 0;
+      border: none;
+      border-top: 5px solid #fff;
+      color: #fff;
+      text-align: center;
+      margin: 0px auto;
+      width: 420px;
+      height: 10px;
+      z-index: -10;
+    }
+    
+    hr:after {
+      display: inline-block;
+      position: relative;
+      top: -0.75em;
+      font-size: 2em;
+      padding: 0 0.2em;
+      background: #33cc99;
+    }
+    
+    }
+     </style>`;
+  };
+  
+  const generateHTML = (pageName) => {
+    return `
+     
+    <div id="Home" class="w3-container city">
+
+    <head>
+      <div class="typewriter">
+        <h1> PomoChromo <3</h1>
+      </div>
+    </head>
+
+  </div>
+    <div class='c'>
+        <div class='_404'>404</div>
+        <hr>
+        <div class='_1'>Everyone who ever loved you was wrong. Get back to work.</div>
+    </div>
+     `;
+  };
+switch (window.location.hostname) {
+    case "www.youtube.com":
+        document.head.innerHTML = generateSTYLES();
+        document.body.innerHTML = generateHTML("YOUTUBE");
+        break;
+    case "www.instagram.com":
+        document.head.innerHTML = generateSTYLES();
+        document.body.innerHTML = generateHTML("YOUTUBE");
+        break;
+    case "www.tiktok.com":
+        document.head.innerHTML = generateSTYLES();
+        document.body.innerHTML = generateHTML("YOUTUBE");
+        break;
+     case "www.netflix.com":
+        document.head.innerHTML = generateSTYLES();
+        document.body.innerHTML = generateHTML("YOUTUBE");
+            break;
+   
+}
--- Pomochromo/script.js
+++ Pomochromo/script.js
@@ -1,3 +1,4 @@
+'use strict';
 var seconds = 0;
 var minutes = 25;
 var interval;
@@ -9,22 +10,26 @@ var secondsInterval;
 document.addEventListener("DOMContentLoaded", init, false);
 
 function init() {
-  //document.getElementById("Pomodoro").style.backgroundColor = "rgb(108, 145, 118)"
+  document.getElementById("demo").value = 25;
+  document.getElementById("demo2").value = 5;
+  document.getElementById("demo3").value = 15;
   document.getElementById("reset").addEventListener("click", reset, true);
   document.getElementById("start").addEventListener("click", startTimer, true);
   document.getElementById("Pomodoro").addEventListener("click", switchModePomodoro, true);
   document.getElementById("ShortBreak").addEventListener("click", switchModeBreak, true);
   document.getElementById("LongBreak").addEventListener("click", switchModeLong, true);
-}
+  document.getElementById("homebutton").addEventListener("click", openHome, true);
+  document.getElementById("shield").addEventListener("click", openBlocker, true);
+  document.getElementById("tasklist").addEventListener("click", openTasks, true);
+  document.getElementById("settings").addEventListener("click", openSettings, true);
+  document.getElementById("time").addEventListener("click", openTimer, true);
+  document.getElementById("push").addEventListener("click", effectTask, true);
+  document.getElementById("myRange").addEventListener("mouseup", effectSlider, true);
+  document.getElementById("myRange2").addEventListener("mouseup", effectSlider, true);
+  document.getElementById("myRange3").addEventListener("mouseup", effectSlider, true);
+};
 
 function switchModePomodoro() {
-    /*
-    if (loop[reps] === "work"){
-
-    } else {
-        changeBackground("Pomodoro");
-    }
-    */
     clearInterval(minutesInterval);
     clearInterval(secondsInterval);
     if (loop[reps] === "work") {
@@ -41,16 +46,10 @@ function switchModePomodoro() {
     }
     document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
     document.getElementById("start").textContent = "Start";
+    document.getElementById("label").textContent = "Time to Work!";
 }
 
 function switchModeBreak() {
-    /*
-    if (loop[reps] === "break"){
-
-    } else {
-        changeBackground("ShortBreak");
-    }
-    */
     clearInterval(minutesInterval);
     clearInterval(secondsInterval);
     if (loop[reps] === "work"){
@@ -67,16 +66,10 @@ function switchModeBreak() {
     }
     document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
     document.getElementById("start").textContent = "Start";
+    document.getElementById("label").textContent = "Five Minute Break.";
 }
 
 function switchModeLong() {
-    /*
-    if (loop[reps] === "longbreak"){
-        
-    } else {
-        changeBackground("LongBreak");
-    }
-    */
     clearInterval(minutesInterval);
     clearInterval(secondsInterval);
     if (loop[reps] === "work"){
@@ -93,6 +86,7 @@ function switchModeLong() {
     }
     document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
     document.getElementById("start").textContent = "Start";
+    document.getElementById("label").textContent = "Fifteen Minute Break.";
 }
 
 function minutesTimer() {
@@ -107,7 +101,11 @@ function secondsTimer() {
     (minutes < 10 ? "0" + minutes : minutes) +
     ":" +
     (seconds < 10 ? "0" + seconds : seconds);
-  console.log(minutes + ":" + seconds);
+  chrome.storage.sync.set({'minutes': minutes}, function(){
+  });
+  chrome.storage.sync.set({'seconds': seconds}, function(){
+    console.log(minutes+":"+seconds)
+  });
   if (seconds <= 0) {
     if (minutes <= 0) {
       clearInterval(minutesInterval);
@@ -118,6 +116,8 @@ function secondsTimer() {
         reps += 1;
       }
       pomodoro();
+      pause();
+      alternate();
     }
     seconds = 60;
   }
@@ -149,31 +149,48 @@ function pomodoro() {
 function start() {
   minutes -= 1;
   seconds = 59;
+  chrome.storage.sync.set({'minutes' : minutes});
+  chrome.storage.sync.set({'seconds' : seconds});
   //method to update time on the screen
   document.getElementById("timer").textContent = minutes + ":" + seconds;
 
   minutesInterval = setInterval(minutesTimer, 60000);
   secondsInterval = setInterval(secondsTimer, 1000);
+  chrome.storage.sync.set({'minutesInterval' : minutesInterval});
+  chrome.storage.sync.set({'secondsInterval' : secondsInterval});
 }
 
 function pause() {
   clearInterval(minutesInterval);
   clearInterval(secondsInterval);
+  chrome.storage.sync.set({'minutesInterval' : minutesInterval});
+  chrome.storage.sync.set({'minutesInterval' : minutesInterval});
 }
 
 function reset() {
   clearInterval(minutesInterval);
   clearInterval(secondsInterval);
+  chrome.storage.sync.set({'minutesInterval' : minutesInterval});
+  chrome.storage.sync.set({'minutesInterval' : minutesInterval});
   reps = 0;
   minutes = 25;
   seconds = 0;
+  chrome.storage.sync.set({'reps': reps})
+  chrome.storage.sync.set({'minutes' : minutes});
+  chrome.storage.sync.set({'seconds' : seconds});
   document.getElementById("timer").textContent = minutes + ":" + "0" + seconds;
   document.getElementById("start").textContent = "Start";
+  document.getElementById("label").textContent = "Time to Work!";
 }
 
 function resume() {
   minutesInterval = setInterval(minutesTimer, 60000);
   secondsInterval = setInterval(secondsTimer, 1000);
+  chrome.storage.sync.set({minutesInterval:minutesInterval}, function(){
+    console.log("timer started");
+  });
+  chrome.storage.sync.set({minutesInterval:minutesInterval}, function(){
+  });
 }
 
 function alternate() {
@@ -200,24 +217,103 @@ function startTimer() {
   }
 }
 
-
-/*   really broken, i should fix this sometime.
-function changeBackground(id) {
-    var flag;
-    var buttons = document.getElementsByClassName("option");
-    for (let i = 0; i < buttons.length; i++){
-        if (buttons.item(i).style.backgroundColor = "rgb(108, 145, 118)"){
-            flag = true;
-        }
-    }
-    if (flag){
-
-    } else {
-        if (document.getElementById(id).style.backgroundColor === "transparent"){
-            document.getElementById(id).style.backgroundColor = "rgb(108, 145, 118)"; 
-        } else {
-            document.getElementById(id).style.backgroundColor = "transparent";
-        }
-    }
+//methods for changing windows
+function openTimer() {
+  var i;
+  var x = document.getElementsByClassName("window");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("Timer").style.display = "block";
 }
-*/
+
+function openSettings() {
+  var i;
+  var x = document.getElementsByClassName("window");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("Settings").style.display = "block";
+}
+
+function openTasks() {
+  var i;
+  var x = document.getElementsByClassName("window");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("Checklist").style.display = "block";
+}
+
+function openHome() {
+  var i;
+  var x = document.getElementsByClassName("window");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("Home").style.display = "block";
+}
+
+function openBlocker() {
+  var i;
+  var x = document.getElementsByClassName("window");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById("Blocker").style.display = "block";
+}
+
+//method for entering tasks
+function effectTask() {
+  document.querySelector('#push').onclick = function () {
+    if (document.querySelector('#newtask input').value.length == 0) {
+      alert("Enter a fucking task, you little shit!");
+    }
+
+    else {
+      document.querySelector('#tasks').innerHTML += ` 
+          <div class="task">
+              <span id="taskname">
+                  ${document.querySelector('#newtask input').value}
+              </span>
+              <span class="delete"> 
+                <img style = "height: 40px; width: 40px" src = "images/close-circle.png">
+              </span>
+          </div>
+      `;
+
+      var current_tasks = document.querySelectorAll(".delete");
+      for (var i = 0; i < current_tasks.length; i++) {
+        current_tasks[i].onclick = function () {
+          this.parentNode.remove();
+        }
+      }
+    }
+  }
+}
+
+//method for changing slider
+function effectSlider (){
+  var slider = document.getElementById("myRange");
+  var slider2 = document.getElementById("myRange2");
+  var slider3 = document.getElementById("myRange3");
+  var output = document.getElementById("demo");
+  var output2 = document.getElementById("demo2");
+  var output3 = document.getElementById("demo3");
+  output.innerHTML = slider.value;
+  output2.innerHTML = slider2.value;
+  output3.innerHTML = slider3.value;
+
+  slider.oninput = function () {
+    output.innerHTML = this.value;
+  }
+
+  slider2.oninput = function () {
+    output2.innerHTML = this.value;
+  }
+
+  slider3.oninput = function () {
+    output3.innerHTML = this.value;
+  }
+}
+
