8f5f42e2d5c420b0809e603603eb60fc227d5eed
vinngo <ngovincent9593@gmail.com>
2023-07-12 10:59:17 -0700

    timer finished

--- Pomochromo/background.js
+++ Pomochromo/background.js
@@ -4,7 +4,6 @@ var reps;
 var curAction;
 var secondsInterval;
 var visible;
-var curAction;
 var workTime;
 var breakTime;
 var longBreakTime;
@@ -13,34 +12,38 @@ const loop = ["work", "break", "work", "break", "work", "longbreak"];
 
 
 chrome.alarms.onAlarm.addListener(alarm => {
-    chrome.storage.local.get(['minutes', 'seconds', 'reps', 'curAction', 'secondsInterval', 'visible',], function(data){
+    chrome.storage.local.get(['minutes', 'seconds', 'reps', 'curAction', 'secondsInterval', 'visible', 'workTime', 'breakTime', 'longBreakTime'], function(data){
         minutes = data.minutes;
         seconds = data.seconds;
         reps = data.reps;
         curAction = data.curAction;
         secondsInterval = data.secondsInterval;
         visible = data.visible;
+        workTime = data.workTime;
+        breakTime = data.breakTime;
+        longBreakTime = data.longBreakTime;
     });
     if (alarm.name === "myTimer" && curAction === "Pause") {
+        console.log(loop[reps]);
         if (seconds <= 0){
             if (minutes <= 0){
-                if (reps == 5) {
-                    reps = 0;
-                } else {
-                reps += 1;
-                }
-                if (loop[reps] === "work"){
-                    chrome.notifications.create(
-                        "work",
-                        {
-                          type: "basic",
-                          iconUrl: "images/PEEPEEPOOPOO.png",
-                          title: 'Period Over',
-                          message: 'Get to Work!'
+              if (reps == 5) {
+              reps = 0;
+              } else {
+              reps += 1;
+              }
+              if (loop[reps] === "work"){
+                  chrome.notifications.create(
+                      "work",
+                       {
+                        type: "basic",
+                        iconUrl: "images/PEEPEEPOOPOO.png",
+                        title: 'Period Over',
+                        message: 'Get to Work!'
                         }
-                      );
-                      chrome.notifications.clear("work");
-                } else {
+                    );
+                    chrome.notifications.clear("work");
+              } else {
                     chrome.notifications.create(
                         "break",
                         {
@@ -54,6 +57,7 @@ chrome.alarms.onAlarm.addListener(alarm => {
                     chrome.notifications.clear("work");
                 }
                 chrome.storage.local.set({'reps': reps});
+                pomodoro();
             } else {
               seconds = 59;
               minutes -= 1;  
@@ -64,22 +68,6 @@ chrome.alarms.onAlarm.addListener(alarm => {
         chrome.storage.local.set({'minutes': minutes, 'seconds': seconds}, function(){
         console.log(minutes+":"+seconds);
         });
-        /*
-        if (seconds <= 0) {
-            if (minutes <= 0) {
-              if (reps == 5) {
-                reps = 0;
-              } else {
-                reps += 1;
-              }
-              alert("Period is over. Press Resume to continue the timer.");
-              chrome.storage.local.set({'reps': reps});
-            }
-            seconds = 60;
-            minutes -=1;
-            pomodoro();
-        }
-        */
         chrome.storage.local.set({'minutes': minutes, 'seconds': seconds, 'reps': reps});
 
     }
@@ -101,7 +89,7 @@ function pomodoro() {
       minutes = workTime--;
       seconds = 0;
     } else if (loop[reps] === "break") {
-      minutes = breakTime;
+      minutes = breakTime--;
       seconds = 0;
     } else {
       minutes = longBreakTime--;
